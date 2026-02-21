e412b6958bdf6101d143f08e8623141acad963d6
Louis Gorgone <louisgorgone@gmail.com>
2026-02-20 20:48:57 -0500

    New code to fix dataframe and a pca

---
+++ code/source_setup_libraries.R
@@ -0,0 +1,16 @@
+# 2026 Github and R Workshop
+# 20260129
+# Set up libraries
+
+library(dplyr)
+library(geomorph)
+library(ggplot2)
+library(ggforce)
+library(ggrepel)
+library(here)
+library(janitor)
+library(readr)
+library(readxl)
+library(stringr)
+library(tidyr)
+library(tidyverse)
